4362721349e02169c504b4290ebaaf3126f4379b
srnfr <stephane@reytan.fr>
2022-06-29 13:48:16 +0200

    restore before sed

--- toEBPF.sh
+++ toEBPF.sh
@@ -9,6 +9,7 @@ PORT_TO_BE_MODIFIED=443
 echo "IP_TO_BE_MODIFIED => $IP_TO_BE_MODIFIED"
 echo "PORT_TO_BE_MODIFIED => $PORT_TO_BE_MODIFIED"
 
+cp calico-ebpf-configmap.yaml.bak calico-ebpf-configmap.yaml
 sed -i'.bak' -e "s/<IP_TO_BE_MODIFIED>/$IP_TO_BE_MODIFIED/" calico-ebpf-configmap.yaml
 sed -i'.bak' -e "s/<PORT_TO_BE_MODIFIED>/$PORT_TO_BE_MODIFIED/" calico-ebpf-configmap.yaml
 
